1bff2716dad4608b189ac6089c74b5998658ef7d
Sarfaraz Ghulam Iraqui <sarfarazghlm@gmail.com>
2018-01-04 01:25:45 +0530

    first version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# gdrive_to_direct
-Generate direct download links for GDrive files
+# GDrive2Direct
+
+Script to get direct download links from **publicly** viewable Google Drive file url
+
+## Usage
+
+`python gdrive2direct link1 link2 link3 ...`
+
+
+Use `-o` flag with a filename.json to get output in a json file
+
+`python gdrive2direct -o output_file link1 link2 link3 ...`
+
+
+
+Note: A publicly viewable file url usually looks like https://drive.google.com/file/d/<random_string>/view?usp=sharing
+
---
+++ gdrive2direct.py
@@ -0,0 +1,46 @@
+#!/bin/python
+import sys
+import json
+
+# Note: Base url may change in future
+base_direct_url = "https://drive.google.com/uc?export=download&id="
+
+def getDirectUrl(public_url=""):
+	parts = public_url.split('/')
+	file_id = ""
+	for part in parts:
+		if "http" in part or "drive.google.com" in part or "view?usp=sharing" in part or len(part) < 8:
+			pass
+		else :
+			file_id = part
+	if file_id == "":
+		error =  "Error: Not a GDrive url"
+		return error
+	else :
+		return base_direct_url + file_id
+
+# Index of url argument
+url_i = 1
+
+if sys.argv[1] == "-o":
+	url_i = 3
+
+# Dictionary to store urls
+direct_urls = {}
+# Counter that will act as key suffix
+i = 1
+while url_i < len(sys.argv):
+	original_url = sys.argv[url_i]
+	direct_urls['link'+str(i)] = getDirectUrl(original_url)
+	i = i + 1
+	url_i = url_i + 1
+
+if sys.argv[1] == '-o':
+	json_out = json.dumps(direct_urls, indent=4)
+	file = open(sys.argv[2],'w')
+	print >> file, json_out
+	print "Output written to " + sys.argv[2]
+
+for key in direct_urls:
+	print direct_urls[key]
+
